af327aa5289ccdbb546417b2337f05ba0508faa2
Patricio Ezequiel Suarez <psuarez@algeiba.com>
2022-06-14 14:50:52 -0300

    agregando delete y edit

---
+++ TablaDePosiciones_Patricio/ViewModels/ViewMatchs/DeleteMatchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TablaDePosiciones_Patricio.Models;
+
+namespace TablaDePosiciones_Patricio.ViewModels.ViewMatchs
+{
+    public class DeleteMatchViewModel
+    {
+        public IEnumerable<Team> Teams { get; set; }
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un equipo")]
+        [Display(Name = "Local")]
+        public int HomeTeamId { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un equipo")]
+        [Display(Name = "Visitante")]
+        public int GuestTeamId { get; set; }
+        [Display(Name = "Pts")]
+        public float HomePoints { get; set; }
+        [Display(Name = "Pts")]
+        public float GuestPoints { get; set; }
+    }
+}
